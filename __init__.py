cff8cf07283f41bc2e448f2fedc438449e115426
rudy-prietno <prietnorudy@gmail.com>
2022-06-27 07:40:37 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from preparation_query import query_pararelize_services
+from preparation_query_when_use_unload_spark import query_pararelize_services
 
 __all__ = [
     'query_pararelize_services'
